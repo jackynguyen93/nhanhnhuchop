ff19677202e4ce70e315b6d202c51dad57c70c5d
Nguyen <jackynguyen93@gmail.com>
2018-12-11 10:47:35 +0700

    init

---
+++ src/game/Game.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="container home-screen">
+        Game Screen
+      </div>
+    );
+  }
+}
+
+export default Game;
---
+++ src/home/Home.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './home.css';
+import {Link} from "react-router-dom";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="container home-screen">
+        <img className="logo" src="./images/logo.png"/>
+        <div className="home-main">
+          <div className="btn-home-content">
+            <button className="btn-home-center" id="btn-home-play">
+              <Link to="/game" />
+              <img src="./images/btn-home-play.png"/>
+            </button>
+            <button className="btn-home-center" id="btn-home-friend">
+              <img src="./images/btn-home-friend.png"/>
+            </button>
+            <button className="btn-home-center" id="btn-home-time">
+              <img src="./images/btn-home-time.png"/>
+            </button>
+          </div>
+          <div className="home-bottom-bar">
+            <button className="btn-home-bottom" id="btn-home-exit">
+              <img src="./images/btn-home-exit.png"/>
+            </button>
+            <button className="btn-home-bottom" id="btn-home-leaderboard">
+              <img src="./images/btn-home-leaderboard.png"/>
+            </button>
+            <button className="btn-home-bottom" id="btn-home-help">
+              <img src="./images/btn-home-help.png"/>
+            </button>
+            <button className="btn-home-bottom" id="btn-home-sound" value="true">
+              <img id="img-home-sound" src="./images/btn-home-sound.png"/>
+            </button>
+            <button className="btn-home-bottom" id="btn-home-avatar">
+              <img src="./images/btn-home-background.png"/>
+              <img className="img-player-avatar"/>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
